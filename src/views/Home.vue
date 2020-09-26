a3bb2840a18248120bab7fded3cf9eb1f00e0b22
li_kangJin <1047505569@qq.com>
2020-09-26 12:07:51 +0800

    文件配置

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-   <div id="read"></div>
+   <span class="icon-bookmark"></span>
   </div>
 </template>
 
@@ -9,12 +9,5 @@ import Epub from 'epubjs'
 global.ePub = Epub
 export default {
   name: 'Home',
-  mounted () {
-    this.book = new Epub('/《围城》 (七十年纪念)  钱钟书.epub')
-    this.book.renderTo('read',{
-      width:window.innerWidth,
-      height:window.innerHeight
-    }).display()
-  }
 }
 </script>
